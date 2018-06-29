e709c1429ba8f39588441174a3c2ed98dba65e9b
Jeff Griffiths <jeffrey_griffiths@hotmail.com>
2018-06-29 18:12:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,15 +18,3 @@ function logWhisper(string)
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string)
-{
-  if (string.toUpercase() === string) {
-    return "YES INDEED!"
-  }
-  else if (string === "I love you, Grandma.") {
-    return "I love you, too."
-  }
-  else {
-    return "I can't hear you!"
-  }
-}
